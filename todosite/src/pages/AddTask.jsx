30c83ce5fe811d23a2f9cfd2db14052e3daee378
karnampraveen <karnampraveen@gmail.com>
2024-05-29 16:53:18 +0100

    Code with JSON Server where Add task works but Display tasks gives error.

--- todosite/src/pages/AddTask.jsx
+++ todosite/src/pages/AddTask.jsx
@@ -1,9 +1,56 @@
-const AddTask = () => {
-    return(
-        <>
-            <h1>This is Add Task page</h1>
-        </>
+import { useState, useEffect } from "react";
+import TaskData from '../data/TaskData.json'
+
+export default function AddTask() {
+    // states
+    const [Task_Name, setTaskName] = useState('')
+    const [Task_Description, setTaskDesc] = useState('')
+    const [Task_Time, setTaskTime] = useState(0)
+    const [Task_Priority, setTaskPriority] = useState('')
+
+
+    //Efects
+
+    // Other functions
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const newTask = { Task_Name, Task_Description, Task_Time, Task_Priority }
+        fetch('http://localhost:8000/Tasks',
+            {
+                method: 'POST',
+                headers: { "Content-Type" : "application/json"},
+                body: JSON.stringify(newTask)
+            }
+        )
+        // TaskData = { ...TaskData, newTask }
+        console.log(TaskData)
+        alert("You have added your task!")
+        setTaskName('')
+        setTaskDesc('')
+        setTaskTime(0)
+        setTaskPriority('')
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>Task Name: </label>
+            <input type="text" required value={Task_Name} onChange={(e) => setTaskName(e.target.value)} />
+            <br /><br /><br />
+            <label>Task Description: </label>
+            <input type="text" required value={Task_Description} onChange={(e) => setTaskDesc(e.target.value)} />
+            <br /><br /><br />
+            <label>Duration in Hours: </label>
+            <input type="number" required value={Task_Time} onChange={(e) => setTaskTime(e.target.value)} />
+            <br /><br /><br />
+            <label>Task Priority: </label>
+            <select required value={Task_Priority} onChange={(e) => setTaskPriority(e.target.value)}>
+                <option value=""></option>
+                <option value="High">High</option>
+                <option value="Medium">Medium</option>
+                <option value="Low">Low</option>
+            </select>
+            <br /><br /><br />
+            <button >Add Task</button>
+        </form>
     );
 }
-
-export default AddTask
--- todosite/src/pages/DisplayTasks.jsx
+++ todosite/src/pages/DisplayTasks.jsx
@@ -1,9 +1,58 @@
-const DisplayTasks = () => {
-    return(
-        <>
-            <h1>This is Display Tasks page</h1>
-        </>
+import { useEffect, useState } from 'react'
+import TaskData from '../data/TaskData.json'
+
+const DisplayTasks = (task) => {
+// const 
+    // const [task, setTask] = useState('')
+
+    // const [Task_Name, setTaskName] = useState('')
+    // const [Task_Description, setTaskDesc] = useState('')
+    // const [Task_Time, setTaskTime] = useState(0)
+    // const [Task_Priority, setTaskPriority] = useState('')
+    const [tasks, setTasks] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:8000/Tasks')
+            .then((response) => response.json())
+            // .then((data)=> setTaskName(data.message))
+            // .then((data)=> setTaskDesc(data.message))
+            // .then((data)=> setTaskTime(data.message))
+            // .then((data)=> setTaskPriority(data.message))
+            .then((data) => setTasks(data.message))
+    }, [])
+
+    return (
+        <div>
+        <h1 className='header'>Tasks</h1>
+
+
+{tasks && tasks?.map((task) =>(
+    <div key={task.id}>
+    <p>{task.Task_Name}</p>
+    <p>{task.Task_Description}</p>
+    <p>{task.Task_Time}</p>
+    <p>{task.Task_Priority}</p>
+    </div>
+))}
+
+
+        </div>         
     );
 }
 
 export default DisplayTasks
+
+            {/* {TaskData.map((Task)=>(
+                <div>
+                <h1 className='header'>{Task.Task_Name}</h1>
+                <h5 className='header'>{Task.Task_description}</h5>
+                <h3 className='header'>{Task.Task_Time}</h3>
+                <h5 className='header'>{Task.Task_Priority}</h5>
+                </div>
+            ))} */}
+
+
+{/* <p>{Task_Name}</p>
+<p>{Task_Description}</p>
+<p>{Task_Time}</p>
+<p>{Task_Priority}</p> */}
